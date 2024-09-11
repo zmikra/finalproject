b1574f7ca443d7c82823715af06fa59dd34aea0b
NatashaYing <natashayingmuniz@gmail.com>
2024-09-11 13:17:37 -0300

    Corrección
    No repetir código de verificación de login en cada js, solamente ponerlo en el init.js

--- js/init.js
+++ js/init.js
@@ -39,3 +39,13 @@ let getJSONData = function(url){
         return result;
     });
 }
+
+// Verificar si la sesión está activa
+if (sessionStorage.getItem("sesionActiva") !== "true") {
+  // Redireccionar a login.html si no está autenticado
+  window.location.href = "login.html";
+}else {
+  // Mostrar el nombre de usuario en la esquina superior derecha
+  var usuario = sessionStorage.getItem("usuario");
+  document.getElementById("nombreUsuario").textContent =  usuario;
+}
--- js/products.js
+++ js/products.js
@@ -1,12 +1,4 @@
-// Verificar si la sesión está activa
-if (sessionStorage.getItem("sesionActiva") !== "true") {
-    // Redireccionar a login.html si no está autenticado
-    window.location.href = "login.html";
-}else {
-    // Mostrar el nombre de usuario en la esquina superior derecha
-    var usuario = sessionStorage.getItem("usuario");
-    document.getElementById("nombreUsuario").textContent =  usuario;
-}
+
 
 async function getproducts(url) {
         return fetch(url)
