6d563547b97ce0eb0ae29b5968a727f9def6d8d4
Micaela Rodríguez <mrodriguez111194@gmail.com>
2024-09-11 13:39:36 -0300

    explicaciones del código

--- js/products.js
+++ js/products.js
@@ -1,3 +1,4 @@
+//Creo las variables similares a las que tenemos en categories.js 
 const ORDER_ASC_BY_PRICE = "PRICE_ASC";
 const ORDER_DESC_BY_PRICE = "PRICE_DESC";
 const ORDER_DESC_BY_RELEVANCE = "RELEVANCE_DESC";
@@ -16,23 +17,23 @@ if (sessionStorage.getItem("sesionActiva") !== "true") {
     document.getElementById("nombreUsuario").textContent = usuario;
 }
 
-/*async function getproducts(url) {
-    return fetch(url)
-        .then(response => response.json())
-        .then(productList => {
-            showProducts(productList.products);*/
-            
+
+/*creo función para hacerle sort a los productos y ordenarlos según los criterios manejados de la entrega.
+Este sort los ordena haciendo uso de una función anónima*/
 function sortProducts(criteria, array) {
     let result = [];
     if (criteria === ORDER_ASC_BY_PRICE) {
+        //organiza los productos del más barato al más caro
         result = array.sort(function(a, b) {
             return a.cost - b.cost;
         });
     } else if (criteria === ORDER_DESC_BY_PRICE) {
+        //organiza los productos del más caro al más barato
         result = array.sort(function(a, b) {
             return b.cost - a.cost;
         });
     } else if (criteria === ORDER_DESC_BY_RELEVANCE) {
+        //organiza los productos según la relevancia (del más vendido al menos vendido)
         result = array.sort(function(a, b) {
             return b.soldCount - a.soldCount;
         });
@@ -65,9 +66,12 @@ function showProducts(productList) {
     document.getElementById("tproducts").innerHTML = productsHTML;
 }
 
+//esta función toma el criterio con el cual vamos a ordenar los productos y el array mismo
 function sortAndShowProducts(sortCriteria, productsArray) {
     currentSortCriteria = sortCriteria;
-
+//si se le pasa la lista de productos (el array), lo guarda en currentProductsArray y después usa la
+//función sortProducts para ordenar según el criterio (currentSortCriteria) y por último los muestra
+//con showProducts
     if (productsArray != undefined) {
         currentProductsArray = productsArray;
     }
@@ -75,8 +79,9 @@ function sortAndShowProducts(sortCriteria, productsArray) {
     currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
     showProducts(currentProductsArray);
 }
-
+//cuando carga la página se ejecuta:
 document.addEventListener("DOMContentLoaded", function() {
+    //toma los datos de catID, luego de recibirlos verifica que estén ok (if) y los muestra con showproducts
     let catId = localStorage.getItem("catID");
 
     getJSONData(PRODUCTS_URL + catId + ".json").then(function(resultObj) {
@@ -85,19 +90,20 @@ document.addEventListener("DOMContentLoaded", function() {
             showProducts(currentProductsArray);
         }
     });
-
+//líneas de código de eventos:
+//al hacer clic ordena los productos de menor a mayor
     document.getElementById("sortPriceAsc").addEventListener("click", function() {
         sortAndShowProducts(ORDER_ASC_BY_PRICE);
     });
-
+//al hacer clic ordena los productos de mayor a menor
     document.getElementById("sortPriceDesc").addEventListener("click", function() {
         sortAndShowProducts(ORDER_DESC_BY_PRICE);
     });
-
+//al hacer clic ordena los productos por relevancia
     document.getElementById("sortByRelevance").addEventListener("click", function() {
         sortAndShowProducts(ORDER_DESC_BY_RELEVANCE);
     });
-
+//al hacer clic limpia los campos de filtro de precio y muestra los productos de nuevo
     document.getElementById("clearRangeFilter").addEventListener("click", function() {
         document.getElementById("rangeFilterPriceMin").value = "";
         document.getElementById("rangeFilterPriceMax").value = "";
@@ -107,7 +113,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
         showProducts(currentProductsArray);
     });
-
+//al hacer clic toma los valores introducidos, los convierte a números y muestra los productos dentro 
+//de ese rango. 
     document.getElementById("rangeFilterPrice").addEventListener("click", function() {
         minPrice = document.getElementById("rangeFilterPriceMin").value;
         maxPrice = document.getElementById("rangeFilterPriceMax").value;
