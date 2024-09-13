4584ca07a453ff8a694a8474d2793842baca4757
Antonella-gif <antonellasalaberry2701@gmail.com>
2024-09-13 16:42:10 -0300

    carousel de imagenes

--- js/product-info.js
+++ js/product-info.js
@@ -20,12 +20,41 @@ document.addEventListener("DOMContentLoaded", function () {
                         </div>
                     </div>
                     <div class="col-12 productImage">
-                        <img src="${product.images[0]}" alt="${product.name}">
+                        <div id="carouselControls" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                ${showImages(product.images)}
+                            </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselControls" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                        </div>
                     </div>
-                `;
+                    `;
 
                 document.getElementById("product-info").innerHTML = productHTML;
             }
         });
     }
 });
+
+function showImages(images) {
+    let htmlImages = "";
+    for(let i = 0; i < images.length; i++) {
+        if(i == 0) {
+            htmlImages += `<div class="carousel-item active">
+                <img src=${images[0]} class="d-block w-100" alt="...">
+            </div>`;
+        } else {
+            htmlImages += `<div class="carousel-item">
+                <img src=${images[i]} class="d-block w-100" alt="...">
+            </div>`;
+        }
+    }
+    return htmlImages;
+}
+
